d5467f6b72570d020ee164252a28b21cdfc7aeae
Dmitriy Grosh <40547597+DmitriyGrosh@users.noreply.github.com>
2024-04-09 19:40:28 +0300

    add default route

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  console.log(1);
+  res.send({ foo: 'booo' });
+});
+
 app.listen(PORT, () => {
   console.log(`Приложение запущено тут: http://localhost:${PORT}`);
 });
